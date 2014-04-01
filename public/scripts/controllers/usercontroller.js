e305a4cdcddb2f8d2ec3d2e36d679cec7271d742
tasb <tiago.bernardo@gmail.com>
2014-04-01 23:23:40 +0100

    UI improvements

--- public/scripts/controllers/usercontroller.js
+++ public/scripts/controllers/usercontroller.js
@@ -1,41 +1,59 @@
 angular.module('meanTodoApp')
-    .controller('UserController', function TodoController($scope, $routeParams, $filter, $window, $location, userService) {
+    .controller('UserController', function TodoController($scope, $window, $location, userService) {
         'use strict';
 
         function init() {
             if ($window.sessionStorage.token) {
                 $location.path("/todoapp");
             }
+        }
+
+        $scope.submitForm = function (isValid) {
+            // check to make sure the form is completely valid
+            $scope.formValid = isValid;
+            if (!isValid) {
+                $scope.message = 'Please insert valid inputs';
+            }
         };
 
         $scope.login = function () {
-            userService.login($scope.username, $scope.password)
-                .success(function (data, status, headers, config) {
-                    $window.sessionStorage.token = data.token;
-                    $window.sessionStorage.userId = data.userId;
-                    $window.sessionStorage.username = $scope.username;
-                    $location.path("/todoapp");
-                })
-                .error(function (data, status, headers, config) {
-                    // Erase the token if the user fails to log in
-                    delete $window.sessionStorage.token;
-                    delete $window.sessionStorage.userId;
-                    delete $window.sessionStorage.username;
+            $scope.message = null;
+            $scope.signupMessage = null;
 
-                    // Handle login errors here
-                    $scope.message = 'Error: Invalid user or password';
-                });
+            if ($scope.formValid) {
+                userService.login($scope.username, $scope.password)
+                    .success(function (data) {
+                        $window.sessionStorage.token = data.token;
+                        $window.sessionStorage.userId = data.userId;
+                        $window.sessionStorage.username = $scope.username;
+                        $location.path("/todoapp");
+                    })
+                    .error(function () {
+                        // Erase the token if the user fails to log in
+                        delete $window.sessionStorage.token;
+                        delete $window.sessionStorage.userId;
+                        delete $window.sessionStorage.username;
+
+                        // Handle login errors here
+                        $scope.message = 'Error: Invalid user or password';
+                    });
+            }
         };
 
         $scope.signup = function () {
-            userService.register($scope.username, $scope.password)
-                .success(function (data, status, headers, config) {
-                    $scope.signupMessage = 'User created. Please login to editing your TODOs';
-                })
-                .error(function (data, status, headers, config) {
-                    // Handle login errors here
-                    $scope.message = 'Error: Invalid user or password';
-                });
+            $scope.message = null;
+            $scope.signupMessage = null;
+
+            if ($scope.formValid) {
+                userService.register($scope.username, $scope.password)
+                    .success(function () {
+                        $scope.signupMessage = 'User created. Please login to editing your TODOs';
+                    })
+                    .error(function () {
+                        // Handle login errors here
+                        $scope.message = 'Error: Invalid user or password';
+                    });
+            }
         };
 
         init();
