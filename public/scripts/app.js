e2442131f2c0a4197eee8da8a5e6845b59a35c24
tasb <tiago.bernardo@gmail.com>
2014-04-01 22:43:59 +0100

    Handled 401 http error

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,37 @@
-/**
- * The main app module
- *
- * @type {angular.Module}
- */
-angular.module('meanTodoApp', ['ngRoute', 'ngQuickDate', 'angular-table'])
+'use strict';
+
+
+angular.module('meanTodoApp', ['ngRoute', 'ngQuickDate', 'angular-table'], 
+    function ($routeProvider, $locationProvider, $httpProvider) {
+    var interceptor = ['$rootScope', '$q', '$location', function (scope, $q) {
+
+        function success(response) {
+            return response;
+        }
+
+        function error(response) {
+            var status = response.status;
+
+            if (status == 401) {
+                delete window.sessionStorage.token;
+                delete window.sessionStorage.userId;
+                delete window.sessionStorage.username;
+                window.location = "./";
+                return;
+            }
+            // otherwise
+            return $q.reject(response);
+
+        }
+
+        return function (promise) {
+            return promise.then(success, error);
+        }
+
+    }];
+    $httpProvider.responseInterceptors.push(interceptor);
+    })
     .config(function ($routeProvider) {
-        'use strict';
 
         $routeProvider.when('/', {
             controller: 'UserController',
@@ -17,4 +43,3 @@ angular.module('meanTodoApp', ['ngRoute', 'ngQuickDate', 'angular-table'])
             redirectTo: '/'
         });
     });
-    
